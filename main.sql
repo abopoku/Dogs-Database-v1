5f6830cc7a5a042791453cb306ceb5a1b8886486
Aba Opoku <aba.opoku@gmail.com>
2022-03-10 10:30:15 -0600

    Update main.sql

--- main.sql
+++ main.sql
@@ -2,18 +2,18 @@
 .mode column      #optional command to make the output look neater and nice.
 .header on        #optional command to make the output look neater and nice.
 
-DROP TABLE IF EXISTS dogs;      #
+DROP TABLE IF EXISTS dogs;      #Deletes a table (line 5-12 is Data Definition Language(DDL) allows creating and modifying database objects such as tables)
 
-CREATE TABLE IF NOT EXISTS dogs(
-  dogid INTEGER NOT NULL PRIMARY KEY,
-  dogname VARCHAR(20) NOT NULL,
-  breed VARCHAR(20),
-  color VARCHAR(50)
+CREATE TABLE IF NOT EXISTS dogs(        #Creates the table 
+  dogid INTEGER NOT NULL PRIMARY KEY,   # the unique identifier of a record in database table, the primary key, important to have. Not Null allows to make sure the column has values in all rows.)
+  dogname VARCHAR(20) NOT NULL,     #adding dogname as a variable strings of characters(VARCHAR)
+  breed VARCHAR(20),                #adding breed as a variable strings of characters(VARCHAR)
+  color VARCHAR(50)                 #adding color as a variable strings of characters(VARCHAR)
 );
 
-INSERT INTO dogs (dogname, breed, color) VALUES
+INSERT INTO dogs (dogname, breed, color) VALUES     #Adding this data to the table, (Data Manipulation Language (DML) is a language used for adding (inserting), deleting, and modifying (updating) data in a database.) 
 ("Azor", "Poodle", "Grey"),("Zuma", "German Shephard","Brown"),("Sunny","Corgi","Black"); 
 
-  INSERT INTO dogs (dogname, breed, color)VALUES ("Peabody","Shih Tzu","White"); #
+  INSERT INTO dogs (dogname, breed, color)VALUES ("Peabody","Shih Tzu","White");  #Adding more data to the table 
 
   SELECT * FROM dogs;   #This allows us to see what our database contains
